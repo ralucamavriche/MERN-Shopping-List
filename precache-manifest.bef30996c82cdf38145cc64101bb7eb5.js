eea1682c442c2614c1b679af75401fe0627ffa8f
Mavriche Raluca <ralucamavriche@gmail.com>
2020-04-25 00:31:48 +0300

    Updates

---
+++ precache-manifest.bef30996c82cdf38145cc64101bb7eb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff14f0384dc83fd70f2ed50caef51b44",
+    "url": "/MERN-Shopping-List/index.html"
+  },
+  {
+    "revision": "d53f3d6fd42a43e98d22",
+    "url": "/MERN-Shopping-List/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1eb49edabbe7ec546c7c",
+    "url": "/MERN-Shopping-List/static/css/main.5ddc4067.chunk.css"
+  },
+  {
+    "revision": "d53f3d6fd42a43e98d22",
+    "url": "/MERN-Shopping-List/static/js/2.73a22d37.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MERN-Shopping-List/static/js/2.73a22d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eb49edabbe7ec546c7c",
+    "url": "/MERN-Shopping-List/static/js/main.a8cfe5e3.chunk.js"
+  },
+  {
+    "revision": "061fecdfb2239e7715fd",
+    "url": "/MERN-Shopping-List/static/js/runtime-main.d403df82.js"
+  }
+]);
